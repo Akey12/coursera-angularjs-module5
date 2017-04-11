9799e5bc74c8123142be5dfbb40670e1bbf25ecc
Kitty Chong <kchong@angus-systems.com>
2017-04-11 17:11:39 -0400

    updated to input fields

--- src/public/signup/signup.controller.js
+++ src/public/signup/signup.controller.js
@@ -4,10 +4,18 @@
     angular.module('public')
            .controller('SignupController', SignupController);
 
-    //SignupController.$inject = [''];
-    function SignupController() {
+    SignupController.$inject = ['SignupService'];
+    function SignupController(SignupService) {
         var signupCtrl = this;
-       // signupCtrl.menuCategories = menuCategories;
+        var firstName = '';
+        var lastName = '';
+        var emailAddress = '';
+        var phoneNumber = '';
+        var favDish = '';
+
+        signupCtrl.Submit = function() {
+
+        };
     }
 
 })();
